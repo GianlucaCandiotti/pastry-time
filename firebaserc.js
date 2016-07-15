39b6e7d53ff4ff23548c50d17f8a8cb3982efa20
Gianluca Candiotti <gianluca.prwlr@gmail.com>
2016-07-15 04:16:55 +0000

    Initial setup and configuration

---
+++ firebaserc.js
@@ -0,0 +1,6 @@
+export default {
+  apiKey: 'AIzaSyD-o1Ice7H28AEWQKnIFO8g2kWnkEyrgpo',
+  authDomain: 'pastrytime-3a4ed.firebaseapp.com',
+  databaseURL: 'https://pastrytime-3a4ed.firebaseio.com',
+  storageBucket: 'pastrytime-3a4ed.appspot.com',
+};
---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    what
+    <ul>
+      <li v-for="user in users">
+        {{ user.name }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import firebase from 'firebase';
+import firebaseConf from 'root/firebaserc.js';
+
+const firebaseApp = firebase.initializeApp(firebaseConf);
+
+const db = firebaseApp.database();
+
+export default {
+  firebase: {
+    users: db.ref('users'),
+  },
+};
+</script>
+
+<style lang="sass" src="./scss/index.scss"></style>
